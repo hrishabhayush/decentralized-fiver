5996134eaddad5acf617b58ac2d060cb1e0b59bd
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-01-09 12:08:43 -0500

    Next Task completed - worker frontend updated

--- worker-frontend/components/NextTask.tsx
+++ worker-frontend/components/NextTask.tsx
@@ -20,37 +20,48 @@ export const NextTask = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         axios.get(`${BACKEND_URL}/v1/worker/nextTask`, {
             headers: {
                 "Authorization": localStorage.getItem("token")
             }
         })
         .then(res => {
-            setCurrentTask(res.data);
+            setCurrentTask(res.data.task);
             setLoading(false);
+        })
+        .catch(e => {
+            setLoading(false);
+            setCurrentTask(null);
         })
     }, [])
 
     if (loading) {
-        return <div>
-            Loading...
+        return <div className="h-screen flex justify-center flex-col">
+            <div className="w-full flex justify-center text-2xl">
+                Loading...
+            </div>
         </div>
     }
 
     if (!currentTask) {
-        return <div>
-            Please check back in some time, no pending tasks available to vote. 
+        return <div className="h-screen flex justify-center flex-col">
+            <div className="w-full flex justify-center text-2xl">
+                Please check back in some time, there are no pending tasks right now
+            </div>
         </div>
     }
+ 
     return <div>
         <div className='text-2xl pt-20 flex justify-center'>
+            {JSON.stringify(currentTask)}
             {currentTask.title}
         </div>
         <div className='flex justify-center pt-8'>
             {currentTask.options.map(option => <Option onSelect={async ()=> {
                 const response = await axios.post(`${BACKEND_URL}/v1/worker/submission`, {
-                    taskId: currentTask.id,
-                    selection: option.id
+                    taskId: currentTask.id.toString(),
+                    selection: option.id.toString()
                 }, {
                     headers: {
                         "Authorization": localStorage.getItem("token")
@@ -58,7 +69,7 @@ export const NextTask = () => {
                 } )
 
                 // refresh the user balance whenever the payment is done 
-                
+
                 const nextTask = response.data.nextTask;
                 if (nextTask) {
                     setCurrentTask(nextTask);
