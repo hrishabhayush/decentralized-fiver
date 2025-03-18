5a5146ec8f78ebd41fb3635835c5673bdd61fce9
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-03-18 16:56:02 -0400

    Logo changed for the website

--- user-frontend/components/Appbar.tsx
+++ user-frontend/components/Appbar.tsx
@@ -34,7 +34,7 @@ export const Appbar = () => {
 
     return <div className="flex justify-between border-b pb-2 pt-2">
         <div className="text-2xl pl-4 flex justify-center">
-            Turkify
+            <img src="/assets/echo.png" alt="Echo Logo" className="h-8 w-auto" />
         </div>
         <div className="text-xl pr-4">
             {publicKey ? <WalletDisconnectButton /> : <WalletMultiButton /> }
