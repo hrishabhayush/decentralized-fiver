fc8c1bf15b4f7b7aa70133def35727f411dc9f67
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-07-26 20:25:23 -0400

    updated config files

--- backend/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOTAL_DECIMALS = exports.WORKER_JWT_SECRET = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = (_a = process.env.JWT_SECRET) !== null && _a !== void 0 ? _a : "";
-exports.WORKER_JWT_SECRET = exports.JWT_SECRET + "worker";
-exports.TOTAL_DECIMALS = 0;
